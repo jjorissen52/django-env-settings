787843a447f42ead3082930986dc57bfec47bea9
jjorissen52 <jjorissen52@gmail.com>
2020-03-10 20:48:07 -0500

    now adds logging and static as well

--- env_settings/management/commands/populate_settings.py
+++ env_settings/management/commands/populate_settings.py
@@ -1,5 +1,5 @@
 import os
-import re
+import sys
 from env_settings import utils
 
 from django.core.management import BaseCommand
@@ -38,10 +38,17 @@ class Command(BaseCommand):
         return {setting: utils.quoted_string(getattr(settings, setting, default)) for setting, default in env_settings_dict.items()}
 
     @staticmethod
-    def remove_setting(setting, settings_lines):
+    def locate_setting_line(setting, settings_lines):
         for i, line in enumerate(settings_lines):
             if line.startswith(setting) and not f"ENV.{setting}" in line:
-                return settings_lines[:i] + settings_lines[i + 1:]
+                return i
+        return -1
+
+    @staticmethod
+    def remove_setting(setting, settings_lines):
+        setting_line = Command.locate_setting_line(setting, settings_lines)
+        if setting_line != -1:
+            return settings_lines[:setting_line] + settings_lines[setting_line + 1:]
         return settings_lines
 
     @staticmethod
@@ -75,10 +82,25 @@ class Command(BaseCommand):
         return settings_lines[:db_settings_start] + db_setting_as_lines + settings_lines[db_settings_end + 1:]
 
     @staticmethod
-    def generate(settings_file, should_write, postgres, gae):
-        with open(os.path.abspath(settings_file), 'r') as f:
-            settings_lines = f.readlines()
+    def update_logging_settings(settings_lines):
+        new_logging_settings_string = render_to_string("logging_settings_template")
+        logging_settings_as_lines = [f'{line}\n' for line in new_logging_settings_string.split("\n")]
+        logging_settings_start, logging_settings_end = Command.dict_bounds("LOGGING", settings_lines)
+        if logging_settings_start == -1:
+            return settings_lines + logging_settings_as_lines
+        return settings_lines[:logging_settings_start] + logging_settings_as_lines + settings_lines[logging_settings_end + 1:]
 
+    @staticmethod
+    def update_static_settings(settings_lines):
+        new_static_settings_string = render_to_string("static_settings_template")
+        static_settings_as_lines = [f'{line}\n' for line in new_static_settings_string.split("\n")]
+        static_url_line = Command.locate_setting_line("STATIC_URL", settings_lines)
+        if static_url_line != -1:
+            return settings_lines[:static_url_line] + static_settings_as_lines + settings_lines[static_url_line + 1:]
+        return settings_lines
+
+    @staticmethod
+    def update_environment_defaults(settings_lines):
         imports_end = 0
         for i, line in enumerate(settings_lines):
             if line.startswith("import os"):
@@ -86,7 +108,8 @@ class Command(BaseCommand):
                 break
 
         if not any("from env_settings import utils" in line for line in settings_lines):
-            settings_lines = settings_lines[:imports_end] + ["from env_settings import utils"] + settings_lines[imports_end:]
+            settings_lines = settings_lines[:imports_end] + ["from env_settings import utils"] + settings_lines[
+                                                                                                 imports_end:]
 
         try:
             environment_defaults_start = settings_lines.index(start_string)
@@ -109,23 +132,40 @@ class Command(BaseCommand):
         for setting in default_settings:
             settings_lines = Command.remove_setting(setting, settings_lines)
 
-        settings_lines = Command.update_database_setting(settings_lines, postgres, gae)
+        environment_defaults_string = render_to_string('env_settings_template',
+                                                       context={'env': default_settings, "start_string": start_string,
+                                                       "end_string": end_string})
+        defaults_as_settings_lines = [f'{line}\n' for line in environment_defaults_string.split("\n")]
+        settings_lines = settings_lines[:environment_defaults_start] + defaults_as_settings_lines + settings_lines[environment_defaults_start:]
+        return settings_lines
 
-        environment_defaults = render_to_string('env_settings_template',
-                                                context={'env': default_settings, "start_string": start_string,
-                                                         "end_string": end_string})
-        settings_str = "".join(settings_lines[:environment_defaults_start]) + f'\n{environment_defaults}\n' + "".join(settings_lines[environment_defaults_start:])
+    @staticmethod
+    def generate(settings_file, should_write, postgres, gae, static, logging):
+        with open(os.path.abspath(settings_file), 'r') as f:
+            settings_lines = f.readlines()
+
+        settings_lines = Command.update_database_setting(settings_lines, postgres, gae)
+        settings_lines = Command.update_environment_defaults(settings_lines)
+        if logging:
+            settings_lines = Command.update_logging_settings(settings_lines)
+        if static:
+            settings_lines = Command.update_static_settings(settings_lines)
+
+        settings_str = "".join(settings_lines)
+
         if should_write:
             with open(settings_file, 'w') as f:
                 f.write(settings_str)
         else:
-            print(settings_str)
+            sys.stdout.write(settings_str)
 
     def add_arguments(self, parser):
         parser.add_argument('-y', action='store_true')
         parser.add_argument('--settings-file')
         parser.add_argument('--postgres', action='store_true')
         parser.add_argument('--gae', action='store_true')
+        parser.add_argument('--static', action='store_true')
+        parser.add_argument('--logging', action='store_true')
 
     def handle(self, *args, **options):
         settings_file = options.get("settings-file", None)
@@ -135,9 +175,10 @@ class Command(BaseCommand):
         if options.get('y', False):
             should_write = True
         else:
-            confirm = input("Are you sure you want to generate settings.py in the project root?"
-                            " This will override any existing version of that file. [y/N] ")
-            should_write = confirm.lower() in ('y', 'yes')
+            # confirm = input("Are you sure you want to generate settings.py in the project root?"
+            #                 " This will override any existing version of that file. [y/N] ")
+            # should_write = confirm.lower() in ('y', 'yes')
+            should_write = False
 
         gae = options.get('gae', False)
         postgres = gae if gae else options.get("postgres", False)
@@ -147,5 +188,7 @@ class Command(BaseCommand):
             env_settings_dict.update(db_settings)
         if gae:
             env_settings_dict["GAE_APPLICATION"] = False
+        static = options.get('static', False)
+        logging = options.get('logging', False)
 
-        self.generate(settings_file, should_write, postgres, gae)
+        self.generate(settings_file, should_write, postgres, gae, static, logging)
