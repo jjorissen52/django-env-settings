1a7db5b27e20bb4def950da47769e6fa21fbc7f9
jjorissen52 <jjorissen52@gmail.com>
2020-10-09 19:28:24 -0500

    add removal of created user and groups to migration

--- env_settings/migrations/0001_initial.py
+++ env_settings/migrations/0001_initial.py
@@ -18,9 +18,21 @@ def add_initial_auth(apps, schema_editor):
         [Group(name=group_name) for group_name in initial_groups if group_name not in existing_groups]
     )
 
-    _ = User.objects.create_superuser(username=settings.INITIAL_ADMIN_USERNAME,
-                                      password=settings.INITIAL_ADMIN_PASSWORD,
-                                      email=settings.INITIAL_ADMIN_EMAIL)
+    if not User.objects.filter(username=settings.INITIAL_ADMIN_USERNAME).exists():
+        _ = User.objects.create_superuser(username=settings.INITIAL_ADMIN_USERNAME,
+                                          password=settings.INITIAL_ADMIN_PASSWORD,
+                                          email=settings.INITIAL_ADMIN_EMAIL)
+    else:
+        print(f"User with username {settings.INITIAL_ADMIN_USERNAME} already exists.")
+
+
+def remove_initial_auth(apps, schema_editor):
+    Group = apps.get_model('auth', 'Group')
+    User = get_user_model()
+    initial_groups = INITIAL_GROUPS
+
+    User.objects.filter(username=settings.INITIAL_ADMIN_USERNAME).delete()
+    Group.objects.filter(name__in=initial_groups).delete()
 
 
 class Migration(migrations.Migration):
@@ -31,4 +43,4 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
-    operations = [migrations.RunPython(add_initial_auth)]
+    operations = [migrations.RunPython(add_initial_auth, remove_initial_auth)]
